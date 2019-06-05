337e2180309c164f921e79ffc7c6e3d83857093d
Ernesto Solares <ernesto.solares.guerrero@gmail.com>
2019-06-04 23:13:33 -0500

    basic funcionality done

--- config/orm.js
+++ config/orm.js
@@ -2,32 +2,39 @@
 var connection = require("./connection.js");
 
 var orm = {
-    selectAll: function (tableInput, cb){
-        var allQuery = "SELECT * FROM " + tableInput + ";";
-
+    selectAll: function (cb){
+        console.log("orm select***");
+        // var allQuery = "SELECT * FROM " + tableInput + ";";
+        var allQuery = "SELECT * FROM burgers;";
         connection.query(allQuery, function(err, result){
             if(err){ throw err;}
 
-            console.log("Selected ---------------");
-            console.log(result);
             cb(result);
         });
     },
-    insertOne: function(tableInput, values, cb){
+    insertOne: function(burger_name, cb){
 
-        var insertQuery = "INSERT INTO " + tableInput + " (?,?) VALUES (?,?);";
+        console.log("orm insert***");
+        var insertQuery = "INSERT INTO burgers (burger_name, devoured) VALUES (?,0);";
 
-        connection.query(insertQuery, ["burger_name", "devoured", values], function(err, result)
+        connection.query(insertQuery, [burger_name], function(err, result)
         {
             if(err){ throw err;}
 
-            console.log("Inserted ---------------");
-            console.log(result);
             cb(result);
-
         });
 
-    }
+    },
+
+    updateOne: function(tableInput, updateColumnName, updateRowVal, searchColumnName, searchRowVal, cb) {
+		var queryString = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
+		connection.query(queryString, [tableInput, updateColumnName, updateRowVal, searchColumnName, searchRowVal], function(err, res) {
+            
+            if(err){ throw err;}
+
+			cb(res);
+		});
+}
 };
 
 module.exports = orm;
--- models/burger.js
+++ models/burger.js
@@ -2,14 +2,20 @@ var orm = require("../config/orm.js");
 
 var burger = {
     selectAll: function(cb){
-        orm.selectAll("burgers", function(res){
+        orm.selectAll(function(res){
             cb(res);
         })
     },
-    insertOne: function(vals, cb){
-        orm.insertOne("burgers", vals, function(res){
+    insertOne: function(burger_name, cb){
+        orm.insertOne(burger_name, function(res){
             cb(res);
         })
+    },
+
+    updateOne: function(id, cb){
+        orm.updateOne("burgers", "devoured", 1, "id", id, function(res){
+            cb(res);
+        });
     }
 };
 
