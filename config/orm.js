652ad1f0054a4cd3b99cf861bf108a012f39550b
Ernesto Solares <ernesto.solares.guerrero@gmail.com>
2019-06-04 19:53:59 -0500

    added connection and orm

---
+++ config/orm.js
@@ -0,0 +1,33 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+var orm = {
+    selectAll: function (tableInput, cb){
+        var allQuery = "SELECT * FROM " + tableInput + ";";
+
+        connection.query(allQuery, function(err, result){
+            if(err){ throw err;}
+
+            console.log("Selected ---------------");
+            console.log(result);
+            cb(result);
+        });
+    },
+    insertOne: function(tableInput, values, cb){
+
+        var insertQuery = "INSERT INTO " + tableInput + " (?,?) VALUES (?,?);";
+
+        connection.query(insertQuery, ["burger_name", "devoured", values], function(err, result)
+        {
+            if(err){ throw err;}
+
+            console.log("Inserted ---------------");
+            console.log(result);
+            cb(result);
+
+        });
+
+    }
+};
+
+module.exports = orm;
